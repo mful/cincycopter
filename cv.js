24ee9000c2bb3751a43de88c46735d74a77d53cd
Matt <matt@roadtrippers.com>
2013-09-07 16:03:09 -0400

    Finished?

--- cv.js
+++ cv.js
@@ -1,23 +1,37 @@
 var cv = require('opencv');
 var arDrone = require('ar-drone');
 var client  = arDrone.createClient();
+var events = require('events');
+var messenger = new events.EventEmitter();
+var huntingState = false;
 
 // access the head camera
 client.config('video:video_channel', 0);
 
 function faceRec(client, cv) {
-  var stream = new cv.ImageStream()
+  var stream = new cv.ImageStream();
 
   stream.on('data', function(matrix){
     matrix.detectObject(cv.FACE_CASCADE, {}, function(err, faces){
       if(err){
-        console.log('error: ' + err)
+        console.log('error: ' + err);
       }
       else {
-        console.log('no err')
+        console.log('no err');
       }
 
-      if()
+      console.log(faces);
+      console.log(faces.length);
+
+      if(faces && faces.length > 0) {
+        messenger.emit('face:found')
+      }
+      else {
+        if(huntingState == false){
+          messenger.emit('face:not:found')
+          huntingState = true;
+        }
+      }
     });
 
   })
@@ -26,20 +40,37 @@ function faceRec(client, cv) {
 }
 
 function goForward() {
+  console.log('go forward');
   client.stop();
-  client.forward(.2);
+  client.stop();
+  client.forward(0.2);
   client.after(1000, function() {
     client.stop();
+    huntingState = false;
   });
 }
 
 function lookForFaces() {
+  console.log('looking for faces');
+  client.stop();
   client.stop();
   client.clockwise(.3);
-  client.after(1000, function() {
-    client.stop();
-  });
 }
+
+function hunt () {
+
+}
+
+function track() {
+
+}
+
+messenger.on('face:found', function() {
+  goForward();
+});
+messenger.on('face:not:found', function() {
+  lookForFaces();
+});
 
 setTimeout(function() {client.land();}, 20000)
 
